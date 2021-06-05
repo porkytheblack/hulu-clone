303e51322b511082834f1920123da7aabbe63cbf
porkytheblack <porkytheblack@gmail.com>
2021-06-06 02:49:19 +0300

    .

--- utils/requests.js
+++ utils/requests.js
@@ -5,10 +5,6 @@ export default {
         title: "Trending",
         url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
     },
-    fetchTopRated:{
-        title: "TopRated",
-        url: `/movie/top-rated?api_key=${API_KEY}&language=en-US`
-    },
     fetchActionMovies:{
         title: "Action",
         url: `/discover/movie?api_key=${API_KEY}&with_genres=28`
